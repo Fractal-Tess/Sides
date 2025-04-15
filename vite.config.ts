e596b51e32206df4fb684815ae790b570f94d347
Fractal-Tess <vgfractal@gmail.com>
2025-04-15 23:25:03 +0300

    Update Vite configuration for GitHub Actions deployment
    - Change base path in vite.config.ts to accommodate GitHub Actions environment.
    - Set GITHUB_ACTIONS environment variable in deploy.yml during the build step.

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/daily-pastel-spark/',
+  base: process.env.GITHUB_ACTIONS ? '/Sides/' : '/',
   server: {
     host: '::',
     port: 8080,

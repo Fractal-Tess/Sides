237206d129d70424e1d9e126c05d8f8ff492e2ea
Fractal-Tess <vgfractal@gmail.com>
2025-04-15 23:33:33 +0300

    Refactor index.html and update Vite configuration for deployment
    - Remove unnecessary script tag from index.html.
    - Update script source path in index.html for consistency.
    - Modify base path in vite.config.ts to use BASE_URL environment variable.
    - Set BASE_URL in deploy.yml for GitHub Actions deployment.

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,8 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: process.env.GITHUB_ACTIONS ? '/Sides/' : '/',
+  base: process.env.BASE_URL || (process.env.GITHUB_ACTIONS ? '/Sides/' : '/'),
+
   server: {
     host: '::',
     port: 8080,
